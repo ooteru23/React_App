1d3e5d2bf2ab9d0394f6601057d19dc28b9612ae
aldo <andryafrinaldo1@gmail.com>
2025-02-13 11:38:38 +0700

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <>
-      <div className="container mt-3">
+      <div className="mt-3">
         <div className="text-center">
           <h3>Home</h3>
         </div>
